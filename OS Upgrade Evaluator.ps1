b8cd4c774790508402893dc1f3cb3e51d453bf40
Will Bixler <william_bixler@yahoo.com>
2019-04-23 18:31:28 -0600

    2.0.0

--- OS Upgrade Evaluator.ps1
+++ OS Upgrade Evaluator.ps1
@@ -1,44 +1,153 @@
-# Get computers
+# Init Variables
 
 $computers = @()
 
-$inputUN = Read-Host -Prompt "Input the domain administrator`'s user name"
-$AdminAccount = "$($env:USERDNSDOMAIN)\$($inputUN)"
-$AdminAccount
-$inputPW = Read-Host -Prompt "Input the domain administrator`'s password"
-$password = ConvertTo-SecureString $inputPW -AsPlainText -Force
-$credentials = New-Object System.Management.Automation.PSCredential $AdminAccount, $password
+# Get network credentials
 
-Write-Host "Getting network computers... This can take a while..." -ForegroundColor Green
+Add-Type -assembly System.Windows.Forms
+$CredentialsForm = New-Object System.Windows.Forms.Form
+$CredentialsForm.Text = "Network Administrator Credentials"
+$CredentialsForm.Width = 320
+$CredentialsForm.Height = 200
+$CredentialsForm.AutoSize = $true
+$CredentialsForm.StartPosition = "CenterScreen"
+
+$instructionsLabel = New-Object System.Windows.Forms.Label
+$instructionsLabel.Location = New-Object System.Drawing.Size(10,20)
+$instructionsLabel.Size = New-Object System.Drawing.Size(300,30)
+$instructionsLabel.Text = "Please input the network administrator`'s login credentials."
+$CredentialsForm.Controls.Add($instructionsLabel)
+
+$UnLabel = New-Object System.Windows.Forms.Label
+$UnLabel.Location = New-Object System.Drawing.Size(10,50)
+$UnLabel.Size = New-Object System.Drawing.Size(80,30)
+$UnLabel.Text = "Username:"
+$CredentialsForm.Controls.Add($UnLabel)
+
+$PwLabel = New-Object System.Windows.Forms.Label
+$PwLabel.Location = New-Object System.Drawing.Size(10,80)
+$PwLabel.Size = New-Object System.Drawing.Size(80,30)
+$PwLabel.Text = "Password:"
+$CredentialsForm.Controls.Add($PwLabel)
+
+$UnInput = New-Object System.Windows.Forms.TextBox
+$UnInput.Location = New-Object System.Drawing.Size(100,50)
+$UnInput.Size = New-Object System.Drawing.Size(200,20)
+if ($env:USERDNSDOMAIN){
+    $UnInput.Text = "$($env:USERDNSDOMAIN)\$($env:USERNAME)"
+} else {
+    $UnInput.Text = $env:USERNAME
+}
+$CredentialsForm.Controls.Add($UnInput)
+
+$PwInput = New-Object System.Windows.Forms.MaskedTextBox
+$PwInput.PasswordChar = "*"
+$PwInput.Location = New-Object System.Drawing.Size(100,80)
+$PwInput.Size = New-Object System.Drawing.Size(200,20)
+$CredentialsForm.Controls.Add($PwInput)
+
+$CurrentCredentialsButton = New-Object System.Windows.Forms.CheckBox
+$CurrentCredentialsButton.Text = "Use current user`'s credentials"
+$CurrentCredentialsButton.Location = New-Object System.Drawing.Size(100,100)
+$CurrentCredentialsButton.Size = New-Object System.Drawing.Size(200,20)
+$CurrentCredentialsButton.Add_Click({
+    if ($CurrentCredentialsButton.Checked) {
+        $UnInput.Enabled = $false
+        $PwInput.Enabled = $false
+    } else {
+        $UnInput.Enabled = $true
+        $PwInput.Enabled = $true
+    }
+
+})
+$CredentialsForm.Controls.Add($CurrentCredentialsButton)
+
+$OkButton = New-Object System.Windows.Forms.Button
+$OkButton.Location = New-Object System.Drawing.Size(160,120)
+$OkButton.Size = New-Object System.Drawing.Size(130,30)
+$OkButton.Text = "OK"
+$OkButton.Add_Click({
+    if ($CurrentCredentialsButton.Checked) {
+        $Script:UseCurrentCredentials = $true
+    } else {
+        $script:inputUN = $UnInput.Text
+        $script:inputPW = $PwInput.Text
+    }
+    $CredentialsForm.Close()
+})
+$CredentialsForm.Controls.Add($OkButton)
+
+[void]$CredentialsForm.ShowDialog()
+if (!$UseCurrentCredentials) {
+    $AdminAccount = $inputUN
+    $AdminPassword = ConvertTo-SecureString $inputPW -AsPlainText -Force
+    $credentials = New-Object System.Management.Automation.PSCredential $AdminAccount, $AdminPassword
+}
+
+# Get computers
+
+Write-Host "Scanning network computers... This can take a while..." -ForegroundColor Green
 $networkComputers = (([adsi]"WinNT://$((Get-WMIObject Win32_ComputerSystem).Domain)").Children).Where({$_.schemaclassname -eq 'computer'})
 
 # Data Collection
 
+$localComputerName = (Get-WmiObject Win32_ComputerSystem).Name
+
 foreach ($RemoteComputer in $networkComputers) {
 
     $RemoteComputerName = $RemoteComputer.Path.Split("/")[3]
-    Write-Host "`nScanning $($RemoteComputerName)" -ForegroundColor Green
+    Write-Host "`nScanning $($RemoteComputerName)..." -ForegroundColor Green
+
+    $computer = New-Object -TypeName psobject
+
+    $computer | Add-Member -MemberType NoteProperty -Name Name -Value $RemoteComputerName
+    $computer | Add-Member -MemberType NoteProperty -Name OS -Value $null
+    $computer | Add-Member -MemberType NoteProperty -Name OSName -Value $null
+    $computer | Add-Member -MemberType NoteProperty -Name Architecture -Value $null
+    $computer | Add-Member -MemberType NoteProperty -Name Processor -Value $null
+    $computer | Add-Member -MemberType NoteProperty -Name Ram -Value $null
+    $computer | Add-Member -MemberType NoteProperty -Name Age -Value $null
+    $computer | Add-Member -MemberType NoteProperty -Name HardDriveSize -Value $null
+    $computer | Add-Member -MemberType NoteProperty -Name HardDriveModel -Value $null
+    $computer | Add-Member -MemberType NoteProperty -Name OSCurrent -Value $false
+    $computer | Add-Member -MemberType NoteProperty -Name ArchitectureValid -Value $false
+    $computer | Add-Member -MemberType NoteProperty -Name ProcessorValid -Value $false
+    $computer | Add-Member -MemberType NoteProperty -Name RamValid -Value $false
+    $computer | Add-Member -MemberType NoteProperty -Name AgeValid -Value $false
+    $computer | Add-Member -MemberType NoteProperty -Name HardDriveValid -Value $false
+    $computer | Add-Member -MemberType NoteProperty -Name SSD -Value $false
+    $computer | Add-Member -MemberType NoteProperty -Name Error -Value $null
     
-    if ($ComputerSystem = Get-WmiObject Win32_ComputerSystem -Impersonation 3 -ComputerName $RemoteComputerName -Credential $credentials -ErrorAction SilentlyContinue) {
+    try {
 
-        $OS = Get-WmiObject Win32_OperatingSystem -Impersonation 3 -ComputerName $RemoteComputerName -Credential $credentials -ErrorAction SilentlyContinue
-        $Processor = Get-WmiObject Win32_processor -Impersonation 3 -ComputerName $RemoteComputerName -Credential $credentials -ErrorAction SilentlyContinue
+        if ($RemoteComputerName -eq $localComputerName) {
+            $ComputerSystem = Get-WmiObject Win32_ComputerSystem -ErrorAction Stop
+            $OS = Get-WmiObject Win32_OperatingSystem -ErrorAction Stop
+            $Processor = Get-WmiObject Win32_processor -ErrorAction Stop
+            $HardDrive = Get-WmiObject Win32_DiskDrive -ErrorAction Stop
+        } elseif ($UseCurrentCredentials) {
+            $ComputerSystem = Get-WmiObject Win32_ComputerSystem -Impersonation 3 -ComputerName $RemoteComputerName -ErrorAction Stop
+            $OS = Get-WmiObject Win32_OperatingSystem -Impersonation 3 -ComputerName $RemoteComputerName -ErrorAction Stop
+            $Processor = Get-WmiObject Win32_processor -Impersonation 3 -ComputerName $RemoteComputerName -ErrorAction Stop
+            $HardDrive = Get-WmiObject Win32_DiskDrive -Impersonation 3 -ComputerName $RemoteComputerName -ErrorAction Stop
+        } else {
+            $ComputerSystem = Get-WmiObject Win32_ComputerSystem -Impersonation 3 -ComputerName $RemoteComputerName -Credential $credentials -ErrorAction Stop
+            $OS = Get-WmiObject Win32_OperatingSystem -Impersonation 3 -ComputerName $RemoteComputerName -Credential $credentials -ErrorAction Stop
+            $Processor = Get-WmiObject Win32_processor -Impersonation 3 -ComputerName $RemoteComputerName -Credential $credentials -ErrorAction Stop
+            $HardDrive = Get-WmiObject Win32_DiskDrive -Impersonation 3 -ComputerName $RemoteComputerName -Credential $credentials -ErrorAction Stop
+        }
 
-        $computer = New-Object -TypeName psobject
-
-        $computer | Add-Member -MemberType NoteProperty -Name Name -Value $ComputerSystem.Name
-        $computer | Add-Member -MemberType NoteProperty -Name OS -Value $OS.Version
-        $computer | Add-Member -MemberType NoteProperty -Name OSName -Value $OS.Name.Split("|")[0]
-        $computer | Add-Member -MemberType NoteProperty -Name Architecture -Value $OS.OSArchitecture
-        $computer | Add-Member -MemberType NoteProperty -Name Processor -Value $Processor.Name
-        $computer | Add-Member -MemberType NoteProperty -Name Ram -Value ([Math]::Round($ComputerSystem.TotalPhysicalMemory/1GB,0))
+        $computer.Name = $ComputerSystem.Name
+        $computer.OS = $OS.Version
+        $computer.OSName = $OS.Name.Split("|")[0]
+        $computer.Architecture = $OS.OSArchitecture
+        $computer.Processor = $Processor.Name
+        $computer.Ram = ([Math]::Round($ComputerSystem.TotalPhysicalMemory/1GB,0))
+        $computer.Age = ([Math]::Round(((New-TimeSpan -Start ($OS.ConvertToDateTime($OS.InstallDate).ToShortDateString()) -End $(Get-Date)).Days / 365), 2))
+        $computer.HardDriveSize = [Math]::Round($HardDrive.Size / 1GB, 0)
+        $computer.HardDriveModel = $HardDrive.Model
 
         # Begin Evaluation
-
-        $computer | Add-Member -MemberType NoteProperty -Name OSCurrent -Value $false
-        $computer | Add-Member -MemberType NoteProperty -Name ArchitectureValid -Value $false
-        $computer | Add-Member -MemberType NoteProperty -Name ProcessorValid -Value $false
-        $computer | Add-Member -MemberType NoteProperty -Name RamValid -Value $false
 
         if ($OS.Version -like "10.*") {
             $computer.OSCurrent = $true
@@ -57,9 +166,21 @@ foreach ($RemoteComputer in $networkComputers) {
             $computer.RamValid = $true
         }
 
+        if ($computer.Age -lt 5.0) {
+            $computer.AgeValid = $true
+        }
+
+        if ($computer.HardDriveSize -ge 100) {
+            $computer.HardDriveValid = $true
+        }
+
+        if ($computer.HardDriveModel -like "*SSD*") {
+            $computer.SSD = $true
+        }
+
         # Begin Output
 
-        if ($computer.ArchitectureValid -and $computer.ProcessorValid -and $computer.RamValid) {
+        if ($computer.ArchitectureValid -and $computer.ProcessorValid -and $computer.RamValid -and $computer.AgeValid) {
             if ($computer.OSCurrent) {
                 Write-Host $computer.Name -ForegroundColor Green
             } else {
@@ -93,15 +214,48 @@ foreach ($RemoteComputer in $networkComputers) {
             Write-Host "$($computer.Ram)GB" -ForegroundColor Red
         }
 
-        $computers += $computer
+        if ($computer.AgeValid) {
+            Write-Host "$($computer.Age) years old" -ForegroundColor Green
+        } else {
+            Write-Host "$($computer.Age) years old" -ForegroundColor Red
+        }
+
+        
+    } catch [System.UnauthorizedAccessException] {
+        Write-Host "Access Denied" -ForegroundColor Red
+        $computer.Error = "Access Denied"
+    } catch [System.Runtime.InteropServices.COMException] {
+        if (Test-Connection $RemoteComputerName -Count 1 -Quiet) {
+            Write-Host "RPC server unavailable" -ForegroundColor Red
+            $computer.Error = "RPC server unavailable"
+        } else {
+            Write-Host "Offline" -ForegroundColor Red
+            $computer.Error = "Offline"
+        }
+    } catch [System.Management.ManagementException] {
+        Write-Host "User credentials cannot be used for local connections" -ForegroundColor Red
+        $computer.Error = "User credentials cannot be used for local connections"
+    } catch {
+        Write-Host "Unknown Error - $($_.Exception.Message) | $($_.Exception.GetType())" -ForegroundColor Red
+        $computer.Error = "Unknown Error - $($_.Exception.Message) | $($_.Exception.GetType())"
     }
+
+    $computers += $computer
 
 }
 
-$Desktop = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
-$Path = "$($Desktop)\OSUpgradeScan.csv"
+Write-Host "Scan complete... Please select where to save the file..."
+
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.SaveFileDialog -Property @{
+    InitialDirectory = [Environment]::GetFolderPath('Desktop')
+    Filter = "CSV (*.csv)|*.csv"
+    FileName = "OSUpgradeEvaluation"
+}
+[void]$FileBrowser.ShowDialog()
+
+$Path = $FileBrowser.FileName
 $computers | Export-Csv -Path $Path
 
-Write-Host "`n`nCSV Exported to Desktop - $($Path)"
-
-pause
+Write-Host "`n`nFile Saved... $($Path)" -ForegroundColor Green
+Write-Host "Done!" -ForegroundColor Green
--- Spreadsheet Builer.ps1
+++ Spreadsheet Builer.ps1
@@ -1,10 +1,20 @@
 # Import Scan
 
-$Desktop = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
-$Path = "$($Desktop)\OSUpgradeScan.csv"
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+    InitialDirectory = [Environment]::GetFolderPath('Desktop')
+    Filter = "CSV (*.csv)|*.csv"
+}
+$FileBrowser.ShowDialog()
+
+$Path = $FileBrowser.FileName
+$FileName = $FileBrowser.SafeFileName.Split(".")[0]
+Write-Host "File: $($FileName)" -ForegroundColor Green
 $computers = Import-Csv -Path $Path
 
 # Create Excel Sheet
+
+Write-Host "Creating Spreadsheet..." -ForegroundColor Green
 
 $excel = New-Object -ComObject excel.application
 $excel.visible = $true
@@ -15,49 +25,118 @@ $UpgradeEvalutation.Name = "Upgrade Evaluation"
 
 # Header
 
+$UpgradeEvalutation.Cells[1,1].EntireRow.Font.Size = 13
+$UpgradeEvalutation.Cells[1,1].EntireRow.Font.Bold = $true
 $UpgradeEvalutation.Cells.Item(1,1) = "Computer Name"
 $UpgradeEvalutation.Cells.Item(1,2) = "Current OS"
 $UpgradeEvalutation.Cells.Item(1,3) = "OS Architecture"
 $UpgradeEvalutation.Cells.Item(1,4) = "Processor"
 $UpgradeEvalutation.Cells.Item(1,5) = "Ram"
+$UpgradeEvalutation.Cells.Item(1,6) = "Age"
+$UpgradeEvalutation.Cells.Item(1,7) = "Hard Drive Capacity"
+$UpgradeEvalutation.Cells.Item(1,8) = "SSD"
+$UpgradeEvalutation.Cells.Item(1,9) = "Suggested Action"
+$UpgradeEvalutation.Cells.Item(1,11) = "Error"
+
 
 # Populate Data
 
 $row = 2
 foreach ($computer in $computers) {
     $UpgradeEvalutation.Cells.Item($row, 1) = $computer.Name
-    if ($computer.OSCurrent -eq "TRUE") {
-        $UpgradeEvalutation.Cells.Item($row,1).Interior.ColorIndex = 4
-    } else {
-        if ($computer.ArchitectureValid -eq "TRUE" -and $computer.ProcessorValid -eq "TRUE" -and $computer.RamValid -eq "TRUE") {
-            $UpgradeEvalutation.Cells.Item($row,1).Interior.ColorIndex = 6
-        } else {
-            $UpgradeEvalutation.Cells.Item($row,1).Interior.ColorIndex = 3
-        }
+    if ($computer.Error) {
+        $UpgradeEvalutation.Cells.Item($row,1).Interior.ColorIndex = 15
+        $UpgradeEvalutation.Cells.Item($row, 11) = "$($computer.Error)"
+        $UpgradeEvalutation.Cells.Item($row,11).Interior.ColorIndex = 3
+        $UpgradeEvalutation.Cells.Item($row,11).Font.ColorIndex = 2
+        $UpgradeEvalutation.Cells.Item($row,11).Font.Bold = $true
     }
     $UpgradeEvalutation.Cells.Item($row, 2) = $computer.OSName
     if ($computer.OSCurrent -eq "TRUE") {
         $UpgradeEvalutation.Cells.Item($row,2).Interior.ColorIndex = 4
-    } else {
+    } elseif ($computer.OSCurrent -eq "FALSE") {
         $UpgradeEvalutation.Cells.Item($row,2).Interior.ColorIndex = 6
     }
     $UpgradeEvalutation.Cells.Item($row, 3) = $computer.Architecture
     if ($computer.ArchitectureValid -eq "TRUE") {
         $UpgradeEvalutation.Cells.Item($row,3).Interior.ColorIndex = 4
-    } else {
+    } elseif ($computer.ArchitectureValid -eq "FALSE") {
         $UpgradeEvalutation.Cells.Item($row,3).Interior.ColorIndex = 3
+        $UpgradeEvalutation.Cells.Item($row,3).Font.ColorIndex = 2
+        $UpgradeEvalutation.Cells.Item($row,3).Font.Bold = $true
     }
     $UpgradeEvalutation.Cells.Item($row, 4) = $computer.Processor
     if ($computer.ProcessorValid -eq "TRUE") {
         $UpgradeEvalutation.Cells.Item($row,4).Interior.ColorIndex = 4
-    } else {
-        $UpgradeEvalutation.Cells.Item($row,4).Interior.ColorIndex = 3
+    } elseif ($computer.ProcessorValid -eq "FALSE") {
+        $UpgradeEvalutation.Cells.Item($row,4).Interior.ColorIndex = 6
     }
     $UpgradeEvalutation.Cells.Item($row, 5) = "$($computer.Ram)GB"
     if ($computer.RamValid -eq "TRUE") {
         $UpgradeEvalutation.Cells.Item($row,5).Interior.ColorIndex = 4
+    } elseif ($computer.RamValid -eq "FALSE") {
+        $UpgradeEvalutation.Cells.Item($row,5).Interior.ColorIndex = 6
     } else {
-        $UpgradeEvalutation.Cells.Item($row,5).Interior.ColorIndex = 3
+        $UpgradeEvalutation.Cells.Item($row, 5) = ""
+    }
+    $UpgradeEvalutation.Cells.Item($row, 6) = "$($computer.Age) years"
+    if ($computer.AgeValid -eq "TRUE") {
+        $UpgradeEvalutation.Cells.Item($row,6).Interior.ColorIndex = 4
+    } elseif ($computer.AgeValid -eq "FALSE") {
+        $UpgradeEvalutation.Cells.Item($row,6).Interior.ColorIndex = 3
+        $UpgradeEvalutation.Cells.Item($row,6).Font.ColorIndex = 2
+        $UpgradeEvalutation.Cells.Item($row,6).Font.Bold = $true
+    } else {
+        $UpgradeEvalutation.Cells.Item($row, 6) = ""
+    }
+    $UpgradeEvalutation.Cells.Item($row, 7) = "$($computer.HardDriveSize)GB"
+    if ($computer.HardDriveValid -eq "TRUE") {
+        $UpgradeEvalutation.Cells.Item($row,7).Interior.ColorIndex = 4
+    } elseif ($computer.HardDriveValid -eq "FALSE") {
+        $UpgradeEvalutation.Cells.Item($row,7).Interior.ColorIndex = 6
+    }
+    $UpgradeEvalutation.Cells.Item($row, 8) = $computer.HardDriveModel
+    if ($computer.SSD -eq "TRUE") {
+        $UpgradeEvalutation.Cells.Item($row,8).Interior.ColorIndex = 4
+    } elseif ($computer.SSD -eq "FALSE") {
+        $UpgradeEvalutation.Cells.Item($row,8).Interior.ColorIndex = 6
+    }
+    if ($computer.ArchitectureValid -eq "FALSE" -or $computer.AgeValid -eq "FALSE") {
+        $UpgradeEvalutation.Cells.Item($row, 9) = "Replace"
+        $UpgradeEvalutation.Cells.Item($row,1).Interior.ColorIndex = 3
+        $UpgradeEvalutation.Cells.Item($row,9).Interior.ColorIndex = 3
+        $UpgradeEvalutation.Cells.Item($row,1).Font.ColorIndex = 2
+        $UpgradeEvalutation.Cells.Item($row,9).Font.ColorIndex = 2
+        $UpgradeEvalutation.Cells.Item($row,1).Font.Bold = $true
+        $UpgradeEvalutation.Cells.Item($row,9).Font.Bold = $true
+    } else {
+        if ($computer.OSCurrent -eq "TRUE" -and $computer.ProcessorValid -eq "TRUE" -and $computer.RamValid -eq "TRUE") {
+            $UpgradeEvalutation.Cells.Item($row, 9) = "OK"
+            $UpgradeEvalutation.Cells.Item($row,1).Interior.ColorIndex = 4
+            $UpgradeEvalutation.Cells.Item($row,9).Interior.ColorIndex = 4
+        } else {
+            $UpgradeEvalutation.Cells.Item($row, 9) = "Upgrade"
+            $UpgradeEvalutation.Cells.Item($row,1).Interior.ColorIndex = 6
+            $UpgradeEvalutation.Cells.Item($row,9).Interior.ColorIndex = 6
+        }
     }
     $row++
+}
+
+# Auto fit cells
+$excel.ActiveSheet.UsedRange.EntireColumn.AutoFit()
+
+Write-Host "Formating complete... Please select where to save the file..."
+
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.SaveFileDialog -Property @{
+    InitialDirectory = [Environment]::GetFolderPath('Desktop')
+    Filter = "Excel Workbook (*.xlsx)|*.xlsx"
+    FileName = $FileName
+}
+$cancel = $FileBrowser.ShowDialog() -eq "cancel"
+
+if (!$cancel) {
+    $Path = $FileBrowser.FileName
+    $UpgradeEvalutation.SaveAs($Path)
 }
